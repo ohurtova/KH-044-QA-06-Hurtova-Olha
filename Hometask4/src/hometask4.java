6d54ee4b09c64f0950cdfd639c3eb43b5eb6bd4a
Hurtova Olha <gurtovayaolenka@gmail.com>
2021-09-13 22:30:41 +0300

    modified file hometask4.java

--- Hometask4/src/hometask4.java
+++ Hometask4/src/hometask4.java
@@ -11,22 +11,29 @@ public class hometask4 {
         for (int i = 0; i < myArray.length; i++) {
             // System.out.println(myArray[i]);
         }
-        System.out.println("Please choose filter:");
+        /*System.out.println("Please choose filter:");
         String[] myList = {"1 - maximum length", "2 - starts with", "3 - ends with", "4 - contains", "0 - exit"};
         for (int i = 0; i < myList.length; i++) {
             System.out.println(myList[i] + " ");
-        }
+        }*/
 
         int filter;
 
-        do { filter = scanner.nextInt();
+        do {
+            System.out.println("Please choose filter:");
+            String[] myList = {"1 - maximum length", "2 - starts with", "3 - ends with", "4 - contains", "0 - exit"};
+            for (int i = 0; i < myList.length; i++) {
+                System.out.println(myList[i] + " ");
+            }
+
+            filter = scanner.nextInt();
 
             switch (filter) {
                 case 1:
                     System.out.println("Maximum length is : ");
                     int num1 = scanner.nextInt();
                     for (int i = 0; i < myArray.length; i++) {
-                        if (myArray[i].length() == num1) {
+                        if (myArray[i].length() <= num1) {
                             System.out.println("Result : - " + myArray[i]);
                         }
                     } break;
